761a1cda9e3b44f0b13fde26046492850307c01e
Fabio Camargo <faccorrea87@gmail.com>
2011-10-05 22:37:41 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyTheater::Application.config.secret_token = '3e17b244cb3654076345bf48548485a086895484a09fc54fda25b6843a4390661ce7babdf96092e269f7f4b309004629a643b95a2851bf921cc462467c766c71'
